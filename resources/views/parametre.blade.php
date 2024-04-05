dc327bd88473719a9679199fc7671ef3bfb6c4bb
Belgina29 <mbizibelgina@gmail.com>
2024-04-05 11:33:35 +0200

    first commit

---
+++ resources/views/parametre.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Accueil</title>
+</head>
+<body>
+    <h1> Bienvenue sur la page parametres </h1>
+</body>
+</html>
---
+++ resources/views/publication.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Accueil</title>
+</head>
+<body>
+    <h1> Bienvenue sur votre page publication </h1>
+</body>
+</html>
+
